b314bdd37cf983b0015f6576ad5645c71e178b50
Nithin V Gopal <nithinvg1495@gmail.com>
2023-04-03 19:15:53 +0530

    removed secrets reliability

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-if [ -d "/etc/secrets/" ]; then
-    echo "loading secrets.."
-    for file in $(ls /etc/secrets/*);
-    do
-        env_var="$(basename -- $file)"
-        while read -rd $'' line
-        do
-            export "$line"
-        done < <(jq -r <<<"$(cat $file)" 'to_entries|map("\(.key)=\(.value)\u0000")[]')
-    done
-else
-    echo "Could not find /etc/secrets directory."
-fi
-
 if [ "$1" = "server" ]; then
     ./server --port $PORT
 else
